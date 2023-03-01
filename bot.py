3549743154d32113e9f0fc7a01ee145d24354fd0
Cake <salahaminsalman@gmail.com>
2023-03-01 14:49:54 -0600

    Add comments

--- bot.py
+++ bot.py
@@ -12,21 +12,27 @@ class MyBot(commands.Bot):
 
     def __init__(self):
         super().__init__(
-            command_prefix='$',
-            intents = discord.Intents.all(),
-            application_id= 1079456113847713923
+            command_prefix='$',                     # Command prefix not used but needed
+            intents = discord.Intents.all(),        # Use all intents lmao
+            application_id= 1079456113847713923     # Bot app ID
         )
 
+    # Setup_hook needed in v2.0+
     async def setup_hook(self):
+        
+        # Load cogs
         for filename in os.listdir('./cogs'):
             if filename.endswith('.py'):
                 print(filename)
                 await self.load_extension(f'cogs.{filename[:-3]}')
 
+        # Sync bot with guild to run slash commands
         await bot.tree.sync(guild = discord.Object(id = 751591466668785734))
         
+    # Runs once bot is actually prepared to do real things
     async def on_ready(self):
         print(f'{self.user} has connected to Discord!')
 
+# Create object and run
 bot = MyBot()
 bot.run(TOKEN)
--- cogs/WelcomeAboard.py
+++ cogs/WelcomeAboard.py
@@ -11,15 +11,18 @@ class WelcomeAboard(commands.Cog):
         self.bot = bot
 
 
+    # Define the 'welcomeaboard' command
     @app_commands.command(
         name = "welcomeaboard",
         description = "Welcome Aboard!"
     )
 
+    # Describe what the 'line' parameter is
     @app_commands.describe(
         line = "Pick voice line"
     )
 
+    # Provide user with voice line choices
     @app_commands.choices(line = [
         Choice(name = "Welcome Aboard!", value = "./sounds/welcomeaboardclip.mp3"),
         Choice(name = "Welcome Aboard! x3", value = "./sounds/welcomeaboard3.mp3"),
@@ -28,35 +31,47 @@ class WelcomeAboard(commands.Cog):
         Choice(name = "Miles to Meal!", value = "./sounds/milestomeal.mp3")
     ])
 
+    # Define the welcomeaboard function that ties to the previously-defined command
     async def welcomeaboard(
         self,
         interaction: discord.Interaction,
-        line: app_commands.Choice[str]
+        line: app_commands.Choice[str]      # Pass app_commands.Choice to access Choice parameters
     ) -> None:
         
+        # Run the function
+
+        # Check if user who used the command is in the voice channel
         if (interaction.user.voice is not None):
-            channel = interaction.user.voice.channel
-            voice = await channel.connect()
-            source = FFmpegPCMAudio(line.value)
+            channel = interaction.user.voice.channel    # Get voice channel
+            voice = await channel.connect()             # Connect to voice channel
+            source = FFmpegPCMAudio(line.value)         # Get file from line path and convert to FFmpeg
+
+            # Have the bot play the audio file and print the choice name
             voice.play(source, after=lambda e: asyncio.run_coroutine_threadsafe(voice.disconnect(), loop=voice.loop))
             await interaction.response.send_message(line.name)
+        
+        # If user is not in voice, send that the user has to be in voice
         else:
             await interaction.response.send_message("You are not in a voice channel")
 
+    # Define the 'follow' command
     @app_commands.command(
         name = "follow",
         description = "Follow Miles to Meal on all platforms!"
     )
 
+    # Define the follow function that ties to previously-defined command
     async def follow(
         self,
         interaction: discord.Interaction
     ) -> None:
 
+        # Send social media information
         await interaction.response.send_message("""Follow Miles to Meal on all platforms!
 Instagram: @milestomeal
 TikTok: @milestomeal""")
 
+# Setup function that creates slash commands and adds to bot
 async def setup(bot: commands.Bot) -> None:
     await bot.add_cog(
         WelcomeAboard(bot),
