622bfb6f7c163a2b5de2e74e02290bc998c5581e
Cake <salahaminsalman@gmail.com>
2023-03-01 15:36:37 -0600

    Add happybirthday command Add new happybirthday command Add happy birthday mr bahrou clip

--- cogs/WelcomeAboard.py
+++ cogs/WelcomeAboard.py
@@ -54,6 +54,44 @@ class WelcomeAboard(commands.Cog):
         else:
             await interaction.response.send_message("You are not in a voice channel")
 
+    # Define the 'Happy Birthday' command
+    @app_commands.command(
+        name = 'happybirthday',
+        description = 'Some happy birthdays are better unspoken'
+    )
+
+    # Describe what the 'line' parameter is
+    @app_commands.describe(
+        line = "Pick voice line"
+    )
+
+    # Provide user with voice line choices
+    @app_commands.choices(line = [
+        Choice(name = "Mr. Bahrou", value = "./sounds/happybirthdaymrbahrou.mp3")
+    ])
+
+    async def happybirthday(
+        self,
+        interaction: discord.Interaction,
+        line: app_commands.Choice[str]
+    ) -> None:
+
+        # Run the function
+
+        # Check if user who used the command is in the voice channel
+        if (interaction.user.voice is not None):
+            channel = interaction.user.voice.channel    # Get voice channel
+            voice = await channel.connect()             # Connect to voice channel
+            source = FFmpegPCMAudio(line.value)         # Get file from line path and convert to FFmpeg
+
+            # Have the bot play the audio file and print the choice name
+            voice.play(source, after=lambda e: asyncio.run_coroutine_threadsafe(voice.disconnect(), loop=voice.loop))
+            await interaction.response.send_message(line.name)
+        
+        # If user is not in voice, send that the user has to be in voice
+        else:
+            await interaction.response.send_message("You are not in a voice channel")
+
     # Define the 'follow' command
     @app_commands.command(
         name = "follow",
